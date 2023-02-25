fb00ae43c7ca99ae5013473b2ceed3e088d7794d
SophiaM03 <86951312+SophiaM03@users.noreply.github.com>
2023-02-25 18:27:46 +0000

    Deploying to gh-pages from @ SophiaM03/goit-js-hw-09@709574ac7973b0ebb7a0df29cc7effbe2db8a348 🚀

---
+++ 01-color-switcher.ffd1077d.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.ffd1077d.js.map
